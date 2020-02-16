b5a96075fbfdc255f3598786606f542328f6cc5c
Erika Shefer <erika.shefer@gmail.com>
2020-02-16 03:17:33 +0300

    all mapping added

--- parser/map_parser.py
+++ parser/map_parser.py
@@ -1,5 +1,5 @@
-input = "../data/floor1_map.txt"
-out = open("floor1_html.txt", "w")
+input = "../data/antresol_map.txt"
+out = open("antresol_html.txt", "w")
 
 for line in open(input, "r"):
     v = line.split()
@@ -14,10 +14,21 @@ for line in open(input, "r"):
         coords[2], coords[0] = coords[0], coords[2]
         coords[3], coords[1] = coords[1], coords[3]
 
-    coords[2] = (abs(coords[2] - coords[0])) / 1000 * 100
-    coords[3] = (abs(coords[3] - coords[1])) / 2040 * 100
-    coords[0] = coords[0] / 1000 * 100
-    coords[1] = coords[1] / 2040 * 100
+    # 600x800 - room
+    # 1000x2040 - floor
+    coords[2] = (abs(coords[2] - coords[0])) / 600 * 100
+    coords[3] = (abs(coords[3] - coords[1])) / 800 * 100
+    coords[0] = coords[0] / 600 * 100
+    coords[1] = coords[1] / 800 * 100
+
+    #only for rooms
+    for i in range(1):
+        if (coords[i] < 50):
+            coords[i] = 50 - (50 - coords[i]) * 0.6
+        else:
+            coords[i] = 50 + (coords[i] - 50) * 0.6
 
     #print("<area target=\"\" alt=\"", title, " title=\"", title, " href=\"", href, " coords=\"", ",".join(coords), " shape=\"poly\">")
-    print("<a href=\"", href, "\" style=\"top: ", coords[1], "%; left: ", coords[0], "%; width: ", coords[2], "%; height: ", coords[3], "%;\"></a>", file=out, sep='')
+
+    # * 0.6 only for rooms
+    print("<a href=\"", href, "\" style=\"top: ", coords[1], "%; left: ", coords[0], "%; width: ", coords[2] * 0.6, "%; height: ", coords[3], "%;\"></a>", file=out, sep='')
